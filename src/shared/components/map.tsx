5ca1fd8334e9859697061927918bd884d63630ba
YoamFargesTCS <yoam.farges@tcs.ch>
2024-07-29 14:48:47 +0200

    Working cluster

--- src/shared/components/map.tsx
+++ src/shared/components/map.tsx
@@ -1,10 +1,11 @@
-import {MapContainer, Marker, Popup} from 'react-leaflet'
+import {MapContainer, Marker} from 'react-leaflet'
 import {TileLayer} from 'react-leaflet'
 import L from 'leaflet'
 import type {GeocodedItem} from '~shared/parser/item';
-import {ImageFetcher} from '~shared/imagefetcher/ItemAdImageFetcher';
 import LBCAPopup from './popup';
 import {useRef} from 'react';
+import MarkerClusterGroup from 'react-leaflet-cluster'
+
 
 const DEFAULT_CENTER = {lat: 46.34, lng: 2.6025};
 const DEFAULT_ZOOM = 4.5;
@@ -17,13 +18,27 @@ const PINICON = new L.Icon({
     iconAnchor: [15, 35],
     shadowAnchor: [7, 1]
 });
+const PINICON_CLUSTER = new L.Icon({
+    iconUrl: chrome.runtime.getURL(`assets/pinicon_cluster.png`),
+    iconRetinaUrl: chrome.runtime.getURL(`assets/pinicon_cluster@2x.png`),
+    shadowUrl: chrome.runtime.getURL('assets/shadow.png'),
+    iconSize: [30, 36],
+    popupAnchor: [-1, -36],
+    iconAnchor: [15, 35],
+    shadowAnchor: [7, 1]
+});
+const createClusterCustomIcon = function (cluster) {
+    return L.divIcon({
+      html: `<p class="count">${cluster.getChildCount()}</p>`,
+      className: 'lbca-marker-cluster',
+      iconSize: L.point(32, 32, true),
+    })
+  }
 
 interface Props {
     geocodedItems: GeocodedItem[]
 }
 export default function LBCAMap({geocodedItems}: Props) {
-    const selectedMarkerRef = useRef(null)
-
     return (
         <MapContainer id="lbca_map" center={DEFAULT_CENTER} zoom={DEFAULT_ZOOM} scrollWheelZoom={true}>
             <TileLayer
@@ -31,15 +46,16 @@ export default function LBCAMap({geocodedItems}: Props) {
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
 
-            {geocodedItems.map((item) => (
-                <Marker
-                    key={item.linkUrl}
-                    position={item.coordinates}
-                    icon={PINICON}
-                    ref={selectedMarkerRef}>
-                    <LBCAPopup item={item} />
-                </Marker>
-            ))}
+            <MarkerClusterGroup chunkedLoading iconCreateFunction={createClusterCustomIcon}>
+                {geocodedItems.map((item, index) => (
+                    <Marker
+                        key={index}
+                        position={item.coordinates}
+                        icon={PINICON}>
+                        <LBCAPopup item={item} />
+                    </Marker>
+                ))}
+            </MarkerClusterGroup>
         </MapContainer>
     )
 }
